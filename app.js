16fd6fa5b798ad7d21d04c21c952795b16c6bde7
Moshe Ben Baruch <MosheBe@eladsystems.co.il>
2023-08-03 18:41:32 +0300

    static middleware

--- app.js
+++ app.js
@@ -1,62 +1,58 @@
 const express = require('express');
 const cors = require('cors');
+const { application } = require('express');
 const app = express();
 
-//list of drinks
-const drinks = [
-  {name: 'coffee', price: 2},
-  {name: 'tea', price: 2},
-  {name: 'lemonade', price: 3},
-  {name: 'coke', price: 2},
+// list of products
+const products = [
+  {
+    id: 1,
+    name: 'product 1',
+    price: 100,
+    description: 'this is product 1',
+    amount: 99
+  },
+  {
+    id: 2,
+    name: 'product 2',
+    price: 200,
+    description: 'this is product 2',
+    amount: 99
+  },
+  {
+    id: 3,
+    name: 'product 3',
+    price: 300,
+    description: 'this is product 3',
+    amount: 99
+  },
 ];
 
 
+// function getProductsPage(req, res) {
+//   res.sendFile(__dirname + '/public/index.html');
+// }
+
+
+// app.get('/style.css', (req, res) => {
+//   res.sendFile(__dirname + '/public/style.css');
+// })
+// app.get('/', getProductsPage);
+
+// app.get('/index.html', getProductsPage);
+  
+
+app.use(cors());
+
+app.use(express.static('public'));
 
 app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-// app.use(cors({
-//   origin: 'http://127.0.0.1:5500',
-// }));
 
-
-// app.use((request, response, next)=> {
-//   let body = '';
-//   request.on('data', (chunk) => {
-//     body += chunk;
-//   });
-//   request.on('end', () => {
-//     request.body = JSON.parse(body);
-//     next();
-//   })
-// })
-
-
-app.use((req, res, next) => {
-  console.log('request recieved: ' +  req.url);
-  console.log('request.body:', req.body);
-  next();
-});
-
-app.use('/:id/abc/:name-:lastname', (request, response) => {
-  console.log(request.url);
-  const fullUrl = 'hello/' + request.url;
-  response.end('hello world');
+app.get('/products', (req, res) => {
+  res.json(products);
 });
 
 
-
-app.use('/hello/world', (req, res) => {
-  res.send('hello world');
-});
-
-
-app.get('/drinks', cors(), (req, res) => {
-  res.json(drinks);
-});
-
-app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/public/about.html');
-});
 
 app.use((req, res) => {
   res.statusCode = 404;
